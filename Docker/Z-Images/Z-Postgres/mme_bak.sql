8da831e5e6bc58eb4dfe5d18c4d0805f8c9ae610
Fred Fields <admin@raspberryfield.life>
2022-02-26 11:00:21 +0100

    postgres info and sql files

---
+++ Docker/Z-Images/Z-Postgres/mme_bak.sql
@@ -0,0 +1,116 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 12.9 (Ubuntu 12.9-0ubuntu0.20.04.1)
+-- Dumped by pg_dump version 12.9 (Ubuntu 12.9-0ubuntu0.20.04.1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: mme; Type: DATABASE; Schema: -; Owner: postgres
+--
+
+CREATE DATABASE mme WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'en_US.UTF-8' LC_CTYPE = 'en_US.UTF-8';
+
+
+ALTER DATABASE mme OWNER TO postgres;
+
+\connect mme
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: names; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.names (
+    id integer NOT NULL,
+    name character varying(255)
+);
+
+
+ALTER TABLE public.names OWNER TO postgres;
+
+--
+-- Name: names_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.names_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.names_id_seq OWNER TO postgres;
+
+--
+-- Name: names_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.names_id_seq OWNED BY public.names.id;
+
+
+--
+-- Name: names id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.names ALTER COLUMN id SET DEFAULT nextval('public.names_id_seq'::regclass);
+
+
+--
+-- Data for Name: names; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.names (id, name) FROM stdin;
+1	Fred
+2	Anna
+3	Maria
+4	Mishka
+\.
+
+
+--
+-- Name: names_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.names_id_seq', 4, true);
+
+
+--
+-- Name: names names_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.names
+    ADD CONSTRAINT names_pkey PRIMARY KEY (id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
